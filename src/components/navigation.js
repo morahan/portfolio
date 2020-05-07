066addc3ade6cf295612a1932a36d7c0c6cd2e04
Michael Morahan <morahan.mike@gmail.com>
2020-05-06 23:14:29 -0600

    added apps to nav

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -11,6 +11,9 @@ export default () => (
       <li className={styles.navigationItem}>
         <Link to="/blog/">Blog</Link>
       </li>
+      <li className={styles.navigationItem}>
+        <Link to="/apps-preview/">Apps</Link>
+      </li>
     </ul>
   </nav>
 )
